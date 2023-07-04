b8dbe40eff5fdaf7644a6fb9ad4b6bf7d7cbcd6a
Harish Kumar P <harish.avenue1@gmail.com>
2023-07-04 20:35:47 +0530

    Update listofcommands.py

--- listofcommands.py
+++ listofcommands.py
@@ -16,3 +16,21 @@ for _ in range(int(input())):
     eval('setA.{0}({1})'.format(*input().split()+['']))
 print(sum(setA))
 
+# Sample Input
+
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 10
+# pop
+# remove 9
+# discard 9
+# discard 8
+# remove 7
+# pop 
+# discard 6
+# remove 5
+# pop 
+# discard 5
+# Sample Output
+
+# 4
